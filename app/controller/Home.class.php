3659ffda5c7fabb3dbb257c734e2f2858801e551
Josua Gonzalez <jdpepi@gmail.com>
2014-12-17 17:20:32 +0100

    Vous êtes qui vous ? Sortez !

--- app/controller/Home.class.php
+++ app/controller/Home.class.php
@@ -7,15 +7,4 @@ class Home extends Controller {
     public function index() {
         $this->afk->view('index', array());
     }
-
-    public function test() {
-        $this->afk->view('index', array());
-    }
-
-    public function yolo() {
-        $user = new UserModel();
-        $users = print_r($user->getUser(1), true);
-        
-        $this->afk->view('yolo', array('users' => $users));
-    }
 }
