048e159bf5072bed133535de024286983d6cfa3f
Josua Gonzalez <jdpepi@gmail.com>
2015-01-15 21:09:12 +0100

    Suppression des fonctions devenues inutiles

--- app/Helpers.class.php
+++ app/Helpers.class.php
@@ -1,19 +1,5 @@
 <?php
 class Helpers {
-    public static function processFilename($filename) {
-    $server = $_SERVER["SERVER_NAME"];
-    $file = dirname($_SERVER['PHP_SELF']);
-    
-    // rediction locahost/nerdbox.fr/ => nerdbox.fr/ (pour le dev en local)
-    if($server == 'localhost') {
-        $server = 'nerdbox.fr';
-        $file = substr($file, 11);
-    }
-
-    $url = 'http://'.$server.$file;
-    return preg_replace("/\.\/(.*)/i", "$url/$1", $filename);
-    }
-
     public static function toFullGender($gender) {
         switch(strtoupper($gender)) {
             case 'M':
@@ -74,13 +60,6 @@ class Helpers {
         exit($message);
     }
 
-    public static function processDBText($text) {
-        $text = utf8_encode($text);
-        $text = htmlentities($text, ENT_NOQUOTES, "UTF-8");
-        $text = htmlspecialchars_decode($text);
-        return $text;
-    }
-
     public static function nameMonth($date) {
         $m = date('m', $date);
         $mois = array('janvier', 'f&eacute;vrier', 'mars', 'avril', 
@@ -95,23 +74,10 @@ class Helpers {
         return $jour[$j];
     }
 
-    public static function detectPodcastMimetype($filename) {
-
-        $mime_types = array(
-            'mp3' => 'audio/mpeg',
-            'wav' => 'audio/x-wav',
-            'wma' => 'audio/x-ms-wma',
-            'ogg' => 'audio/ogg',
-            'oga' => 'audio/ogg',
-            'spx' => 'audio/ogg',
-            'flac' => 'audio/flac',
-        );
-
-        $ext = strtolower(array_pop(explode('.',$filename)));
-        if (array_key_exists($ext, $mime_types)) {
-            return $mime_types[$ext];
-        }
-        
-        return 'application/octet-stream';
+    public static function slugify($str) {
+        $str = strtr($str, 
+          'ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðòóôõöùúûüýÿ', 
+          'AAAAAACEEEEIIIIOOOOOUUUUYaaaaaaceeeeiiiioooooouuuuyy');
+        return strtolower(preg_replace('/([^.a-z0-9]+)/i', '-', $str));
     }
 }
