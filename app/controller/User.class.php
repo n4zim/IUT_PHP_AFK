3947419b668b268936bc2b6d2f1c7323e4e7f383
Josua Gonzalez <jdpepi@gmail.com>
2015-01-16 15:41:52 +0100

    Affichage du profil

--- app/controller/User.class.php
+++ app/controller/User.class.php
@@ -26,4 +26,28 @@ class User extends Controller {
             )
         );
     }
+
+    public function profile($args) {
+        Login::setGoto('user', 'profile');
+        Login::checkIfLogguedIn();
+
+        // if no id is supplied, we show the profile of the loggued in user
+        if(empty($args['id'])) {
+            $args['id'] = $_SESSION['u.id'];
+        } else {
+            $args['id'] = intval($args['id']);
+        }
+
+        $usermodel = new UserModel();
+        $data = array();
+        $data['user'] = $usermodel->getUser(intval($args['id']));
+        $data['user']['FactionLink'] = Helpers::makeUrl('faction', 'view', array('id' => $data['user']['Faction']));
+        $data['isMine'] = ($_SESSION['u.id'] == intval($args['id']));
+        $data['lienEdit'] = Helpers::makeUrl('user', 'edit');
+
+        if(empty($data['user']))
+            exit('utilisateur introuvable');
+
+        $this->afk->view('user/profile', $data);
+    }
 } 
