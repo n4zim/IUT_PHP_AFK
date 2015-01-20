34cf7c0963531388a4c3359bab21568c5cd98f59
Josua Gonzalez <jdpepi@gmail.com>
2015-01-20 20:20:52 +0100

    Pagination + nombre d'events

--- app/model/EventModel.class.php
+++ app/model/EventModel.class.php
@@ -4,22 +4,60 @@ class EventModel extends Model {
         parent::__construct();
     }
 
-    public function getEvents($id = null, $allTime = false) {
+    /**
+     * Get upcoming events
+     * 
+     * @param $id Event id, shows all upcoming event if null or unspecified
+     * @param $allTime Show passed events if true. False by default.
+     * @param $page Page number, do not paginate if null.
+     * 
+     * @return Associative Array containing fields from Event table
+     **/
+    public function getEvents($id = null, $allTime = false, $page = null) {
         $clauseId = (isset($id)) ? ' AND `Id` = :id' : '';
         $req = 'SELECT `Id`,  `Organizer`,  `Titre`,  `Description`,  `Image`,  `Place`,  UNIX_TIMESTAMP(`PostDate`) AS `PostDate`,  UNIX_TIMESTAMP(`EventDate`) AS `EventDate`,  `Reward`
                 FROM `Event`
                 WHERE `EventDate` > :eventDate '.$clauseId.'
                 ORDER BY `EventDate` DESC';
-
-        $data = array(':eventDate' => Helpers::formatSQLDate(time()));
-        if($allTime) $data[':eventDate'] = Helpers::formatSQLDate(0);
-        if(isset($id)) $data[':id'] = $id;
+        
+        if(isset($page))
+            $req .= ' LIMIT :min, :max';
 
         $statement = $this->db->prepare($req);
-        $statement->execute($data);
+
+        if(isset($page)) {
+            $min = $page * Config::$listing['usersPerPage'];
+            $statement->bindValue('min', $page * Config::$listing['usersPerPage'], PDO::PARAM_INT);
+            $statement->bindValue('max', $min + Config::$listing['usersPerPage'], PDO::PARAM_INT);
+        }
+
+        if(isset($id)) $statement->bindValue('id', $id);
+        if($allTime && empty($id)) $statement->bindValue('eventDate', Helpers::formatSQLDate(0));
+        else $statement->bindValue('eventDate', Helpers::formatSQLDate(time()));
+
+        $statement->execute();
 
         $result = $statement->fetchAll();
         if(isset($id)) $result = $result[0];
         return $result;
+    }
+
+    /**
+     * Counts upcoming events
+     * 
+     * @param $allTime Count passed events if true. False by default.
+     * @return Number
+     **/
+    public function countEvents($allTime = false) {
+        $req = 'SELECT COUNT(`Id`) AS `Count`
+                FROM `Event`
+                WHERE `EventDate` > :eventDate';
+
+        $statement = $this->db->prepare($req);
+        $statement->bindValue('eventDate', Helpers::formatSQLDate( ($allTime) ? time() : 0 ));
+        $statement->execute();
+
+        $result = $statement->fetch();
+        return $result['Count'];
     }
 }
