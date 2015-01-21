e21340be9417fe78fba27290a73e01161a56d93d
Josua Gonzalez <jdpepi@gmail.com>
2015-01-21 13:25:15 +0100

    Form pour écrire un message. Détail du message, marquer comme lu, compteur de non lus.

--- app/model/PMModel.class.php
+++ app/model/PMModel.class.php
@@ -4,16 +4,19 @@ class PMModel extends Model {
         parent::__construct();
     }
 
-    public function getPMs($user) {
-        $req = 'SELECT `PM`.`Id`, `Sender`, `Recipient`, `SendDate`, `Read`, `FileName`,
+    public function getPMs($user, $pmId = null) {
+        $req = 'SELECT `PM`.`Id`, `Sender`, `Recipient`, `R`.`Id` AS `RecipientId`, `SendDate`, `Read`, `FileName`,
                     `S`.`UserName` AS `SenderName`, `R`.`UserName` AS `RecipientName`
                 FROM `PM`
                 JOIN `User` AS `S` ON `PM`.`Sender` = `S`.`Id`
                 JOIN `User` AS `R` ON `PM`.`Recipient` = `R`.`Id`
-                WHERE `Recipient` = :user OR `Sender` = :user';
-                
+                WHERE (`Recipient` = :user OR `Sender` = :user)'.(isset($pmId) ? ' AND `PM`.`Id` = :pmId' : '');
+        
+        $data = array(':user' => $user);
+        if(isset($pmId)) $data[':pmId'] = $pmId;
+
         $st = $this->db->prepare($req);
-        $st->execute(array(':user' => $user));
+        $st->execute($data);
         $rs = $st->fetchAll();
 
         foreach ($rs as &$message) {
@@ -30,17 +33,21 @@ class PMModel extends Model {
                 FROM `PM`
                 JOIN `User` AS `S` ON `PM`.`Sender` = `S`.`Id`
                 JOIN `User` AS `R` ON `PM`.`Recipient` = `R`.`Id`
-                WHERE `Recipient` = :user';
+                WHERE `Recipient` = :user
+                ORDER BY `SendDate` DESC';
 
         $st = $this->db->prepare($req);
         $st->execute(array(':user' => $user));
         $rs = $st->fetchAll();
 
-        foreach ($rs as &$message) {
-            $message = $this->parseMessage($message);
+        $msgs = array();
+
+        foreach ($rs as $message) {
+            $m = $this->parseMessage($message);
+            if(!empty($m)) $msgs[] = $m;
         }
 
-        return $rs;
+        return $msgs;
     }
 
     public function getSent($user) {
@@ -49,20 +56,26 @@ class PMModel extends Model {
                 FROM `PM`
                 JOIN `User` AS `S` ON `PM`.`Sender` = `S`.`Id`
                 JOIN `User` AS `R` ON `PM`.`Recipient` = `R`.`Id`
-                WHERE `Sender` = :user';
+                WHERE `Sender` = :user
+                ORDER BY `SendDate` DESC';
         $st = $this->db->prepare($req);
         $st->execute(array(':user' => $user));
         $rs = $st->fetchAll();
 
-        foreach ($rs as &$message) {
-            $message = $this->parseMessage($message);
+        $msgs = array();
+
+        foreach ($rs as $message) {
+            $m = $this->parseMessage($message);
+            if(!empty($m)) $msgs[] = $m;
         }
 
-        return $rs;
+        return $msgs;
     }
 
     private function parseMessage($data) {
-        $message = file_get_contents(Config::$path['pm'].'/'.$data['FileName']);
+        $fname = Config::$path['pm'].'/'.$data['FileName'];
+        if(!file_exists($fname)) return null;
+        $message = file_get_contents($fname);
 
         return array(
             'Id' => $data['Id'],
@@ -72,14 +85,15 @@ class PMModel extends Model {
             'Recipient' => $data['RecipientName'],
             'Read' => ($data['Read'] == 'Y') ? true : false,
             'Message' => $message,
-            'Timestamp' => strtotime($data['SendDate'])
+            'Timestamp' => strtotime($data['SendDate']),
+            'Url' => Helpers::makeUrl('pm', 'view', array('id' => $data['Id']))
         );
 
     }
 
     public function sendPM($from, $to, $message) {
         $req = 'INSERT INTO `PM` (`Sender`, `Recipient`, `SendDate`, `FileName`, `Read`) VALUES (?, ?, ?, ?, \'N\')';
-        
+
         if($from == $to) return false;
 
         $filename = uniqid().'.pm';
@@ -90,6 +104,23 @@ class PMModel extends Model {
 
         $st = $this->db->prepare($req);
         $st->execute(array($from, $to, Helpers::formatSQLDate(time()), $filename));
+        return $this->db->lastInsertId();
+    }
 
+    public function countUnread($user) {
+        $st = $this->db->prepare('SELECT COUNT(`Id`) AS `Count` FROM `PM` WHERE `Recipient` = ? AND `Read` = \'N\'');
+        $st->execute(array($user));
+        return $st->fetch()['Count'];
+    }
+
+    public function markRead($message, $read = true) {
+        $status = ($read) ? 'Y' : 'N';
+        $st = $this->db->prepare('UPDATE `PM` SET `Read` = ? WHERE `Id` = ?');
+        $st->execute(array($status, $message));
+    }
+
+    public function deletePM($id) {
+        $st = $this->db->prepare('DELETE FROM `PM` WHERE `Id` = ?');
+        $st->execute(array($user));
     }
 }
