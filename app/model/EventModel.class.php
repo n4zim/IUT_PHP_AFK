0b2faeadbdf7cad2ce81bbd4995bb0c6667807bf
Josua Gonzalez <jdpepi@gmail.com>
2015-01-21 01:31:21 +0100

    Permet de préciser un ordre de sélection ainsi qu'un nombre max à récupérer

--- app/model/EventModel.class.php
+++ app/model/EventModel.class.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * Manages tables :
+ *  Event
+ *  EventEntrant
+ **/
 class EventModel extends Model {
     public function __construct() {
         parent::__construct();
@@ -7,21 +12,41 @@ class EventModel extends Model {
     /**
      * Get upcoming events
      * 
+     * If I was to redo this function, I would use an array instead of this huuuge number of parameters.
+     * It would be a little less confusing I think.
+     * WHY CAN'T WE SURCHARGE FUNCTIONS IN PHP :(...
+     * 
      * @param $id Event id, shows all upcoming event if null or unspecified
      * @param $allTime Show passed events if true. False by default.
      * @param $page Page number, do not paginate if null.
-     * @param $currentUser If supplied, will also add a field to say if this User (id) is subscribed to the event or not
+     * @param $checkForUser If supplied, will also add a field to say if this User (id) is subscribed to the event or not
+     * @param $limit Number of events to get by page, defaults to config if unset
+     * @param $order How to order events, possible values : ('eventdate', 'score', 'latest')
      * 
      * @return Associative Array containing fields from Event table
      **/
-    public function getEvents($id = null, $allTime = false, $page = null, $checkForUser = null) {
+    public function getEvents($id = null, $allTime = false, $page = null, $checkForUser = null, $limit = null, $order = '') {
         $clauseId = (isset($id)) ? ' AND `Id` = :id' : '';
         $clauseUser = (isset($checkForUser)) ? ', IF(`EventEntrant`.`User` IS NULL, 0, 1) AS `Subscribed`' : '';
         $clauseUser2 = (isset($checkForUser)) ? ' LEFT JOIN `EventEntrant` ON `EventEntrant`.`Event`= `Id` AND `EventEntrant`.`User` = :userId' : '';
         $req = 'SELECT `Id`,  `Organizer`,  `Titre`,  `Description`,  `Image`,  `Place`,  UNIX_TIMESTAMP(`PostDate`) AS `PostDate`,  UNIX_TIMESTAMP(`EventDate`) AS `EventDate`,  `Reward` '.$clauseUser.'
                 FROM `Event`'.$clauseUser2.'
                 WHERE `EventDate` > :eventDate '.$clauseId.'
-                ORDER BY `EventDate` DESC';
+                ORDER BY ';
+
+        switch ($order) {
+            case 'latest':
+                $req .= '`PostDate` DESC';
+                break;
+
+            case 'score':
+                $req .= '`Score` DESC, `EventDate` DESC';
+                break;
+            
+            default:
+                $req .= '`EventDate` DESC';
+                break;
+        }
         
         if(isset($page))
             $req .= ' LIMIT :min, :max';
@@ -29,9 +54,10 @@ class EventModel extends Model {
         $statement = $this->db->prepare($req);
 
         if(isset($page)) {
-            $min = $page * Config::$listing['usersPerPage'];
-            $statement->bindValue('min', $page * Config::$listing['usersPerPage'], PDO::PARAM_INT);
-            $statement->bindValue('max', $min + Config::$listing['usersPerPage'], PDO::PARAM_INT);
+            $perPage = (empty($limit) ? Config::$listing['eventsPerPage'] : $limit);
+            $min = $page * $perPage;
+            $statement->bindValue('min', $page * $perPage, PDO::PARAM_INT);
+            $statement->bindValue('max', $min + $perPage, PDO::PARAM_INT);
         }
 
         if(isset($id)) $statement->bindValue('id', $id);
